fce1cad1aa6922844da849887381beb751c79c99
Marius Lindvall <marius@varden.info>
2016-03-18 21:05:31 +0100

    Update to Minecraft 1.9

--- src/main/java/info/varden/adequacy/forge/Adequacy.java
+++ src/main/java/info/varden/adequacy/forge/Adequacy.java
@@ -10,7 +10,7 @@ public class Adequacy {
     public static final String MODID = "adequacy";
     public static final String VERSION = "1.0";
     public static final String NAME = "Adequacy";
-    public static final String MCVERSION = "1.8";
+    public static final String MCVERSION = "1.9";
     
     /**
      * Logs information to the console.
